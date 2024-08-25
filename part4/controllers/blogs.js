4de1980b887c11a7c3cbf1b182f561c56a80398b
mordi hanzin <94485980+hanzinisthebest@users.noreply.github.com>
2024-08-25 10:38:09 +0300

    finshed part 4.c

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,28 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 blogRouter.post('/', async (request, response) => {
     const body = request.body
+    const user = await User.findById(body.userId)
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: user._id
     })
     if (!blog.likes) blog.likes = 0
-    const result = await blog.save()
+    const savedBlog = await blog.save()
 
-    response.status(201).json(result)
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
 })
 
 blogRouter.get('/:id', async (request, response) => {
